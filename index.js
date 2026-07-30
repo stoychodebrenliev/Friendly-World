cb33064068c6cac8f85c3a7e3e137176615db906
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-07-30 15:18:55 +0200

    Fixes after adding resources

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
 import { engine } from 'express-handlebars'
+
 import routes from './src/routes.js';
 import authMiddleware from './src/middlewares/authMiddleware.js';
 
 const app = express();
 const PORT = 3000;
-
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-app.use(express.static('public'));
-app.use(cookieParser());
-
-app.use(authMiddleware);
-app.use(routes);
 
 app.engine('hbs', engine({
     extname: 'hbs'
@@ -21,6 +14,15 @@ app.engine('hbs', engine({
 
 app.set('view engine', 'hbs');
 app.set('views', './src/views')
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(authMiddleware);
+
+
 
 app.use(routes);
 
